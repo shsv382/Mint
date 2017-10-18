0e7bafdf82967b1b6ea04ac0452780b67c71be66
shsv382 <shsv382@gmail.com>
2017-10-18 21:53:54 +0400

    Added confirmation-validation and submit-block

--- script.js
+++ script.js
@@ -47,17 +47,20 @@ var validation = function() {
 
 var form = document.forms[0];
 var inputs = form.querySelectorAll("input");
-
+var submit = form.querySelector("input[type='submit']") || form.querySelector("button[type='submit']");
+submit.disabled = true;
 
 function valid() {
   var addValClass = function(bool, i) {
       if (bool) {
         inputs[i].classList.add("validation-error");
+        inputs[i].valid = false;
         return true;
       }
       else {
         inputs[i].classList.remove("validation-error");
         inputs[i].classList.add("validation-success");
+        inputs[i].valid = true;
         return false;
       }
   }
@@ -67,7 +70,7 @@ function valid() {
       if(addValClass(inputs[i].value == "", i)) continue;
     }
 
-    if (inputs[i].type == "email") {
+    if (inputs[i].type.toLowerCase() == "email") {
       var regExp = new RegExp("^.+@.+\..+$","");
       if(addValClass(!regExp.test(inputs[i].value), i)) continue;
     } 
@@ -75,7 +78,24 @@ function valid() {
     if (inputs[i].type.toLowerCase() == "password") {
       if(addValClass(inputs[i].value.length < 6, i)) continue;
     }  
+
+// Валидация подтверждения - если confirmation - следующий элемент
+    if (inputs[i].name.indexOf("confirm") >= 0 && inputs[i].name.indexOf(inputs[i - 1].name) >= 0) {
+        if (addValClass((inputs[i].value != inputs[i - 1].value), i)) continue;
+      
+    }
   }
+
+
+// Блокировка кнопки submit
+  var counter = 0;
+
+  for (var i = 0; i < inputs.length; i++) {
+    if (inputs[i].valid == true) counter++;
+  }
+
+  submit.disabled = (counter == inputs.length - 1) ? false : true;
+
 }
 
 document.onkeyup = valid;
